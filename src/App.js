0e7a4d5e5491c1cf42b24b4652f55a5d438606c5
wajdiferjani <wajdi.ferjani@esprit.tn>
2020-10-31 10:14:15 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { render } from '@testing-library/react';
+import wajdi from './wajdi.jpg';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends React.Component {
+
+  state = {
+    fullName:"Wajdi",
+    bio :"Bonjour",
+    imgSrc : wajdi ,
+    profession :"Etudiant",
+    show : false,
+    date : ""
+
 }
+
+
+componentDidMount() {
+  setInterval(() => {
+    let dateNow = new Date()
+    dateNow = dateNow.getHours() + ' : ' + dateNow.getMinutes()
+    this.setState({ date : dateNow})
+
+  },1000)
+  
+}
+
+  render(){
+  return (
+    <>
+    { this.state.show && (<>
+     <h2>Full Name : {this.state.fullName}</h2>
+     <h2>Bio : {this.state.bio}</h2>
+     <img src={this.state.imgSrc} />
+     <h2>Profession : {this.state.profession}</h2>
+     
+        </>)
+    }
+    
+    <button onClick={() => {this.setState({show: !this.state.show})}}> Show</button>
+    <h2>date : {this.state.date}</h2>
+</> 
+  )}
+
+}
+
 
 export default App;
